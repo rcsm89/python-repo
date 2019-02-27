5ebb2981d72b1a614951cd193537c6540682e86b
punisher077 <arthur.mts@gmail.com>
2019-02-27 08:25:40 -0300

    Correção questão 5

--- listaCondicional3/q5.py
+++ listaCondicional3/q5.py
@@ -2,10 +2,13 @@ p1 = (int(input("Peso 1: ")))
 c1 = (int(input("Comp. 1: ")))
 p2 = (int(input("Peso 2: ")))
 c2 = (int(input("Comp. 2: ")))
-if(p1*c1 == p2*c2):
-    print("Gangorra equilibrada")
-elif(p1*c1 > p2* c2):
-    print("Gangorra desequilibrada para a esquerda")
-else:
-    print("Gangorra desequilibrada para a direita")
+if(p1>=10) and (p1<=100) and (c1 >=10) and (c1<=100) and (c2>=10) and (c2<=100):
 
+    if(p1*c1 == p2*c2):
+        print("Gangorra equilibrada")
+    elif(p1*c1 > p2* c2):
+        print("Gangorra desequilibrada para a esquerda")
+    else:
+        print("Gangorra desequilibrada para a direita")
+else:
+    print("Valores invalidos!")
