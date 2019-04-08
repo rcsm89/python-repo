0db6034c8cf99257c3d03876e7366da432eb8be7
puniser077 <punisher077@github.com>
2019-04-08 15:46:53 -0300

    terminei a de lista

--- roteiro3/maisumadelista.py
+++ roteiro3/maisumadelista.py
@@ -1,9 +1,13 @@
 n = int(input())
-l = []
-s = 0
-for i in range(n):
-    l.append(int(input()))
-print(n//2)
-for j in range(n//2):
-    s+= (l[j]-l[(n-(j+1))])**2
-print(s)
+if(n<0):
+    print("O valor informado para N foi negativo")
+elif(n==0):
+    print("Lista vazia - O valor de S eh zero")
+else:
+    l,s= [], 0
+    for i in range(n):
+        l.append(int(input()))
+    if(n%2!=0): l.insert(n//2, 0)
+    for j in range((len(l)//2)):
+        s+= (l[j]-l[-(j+1)])**2
+    print("S = "+str(s))
