9c525d6f902ab08845265cdb130f50d8fcdcdd8e
puniser077 <punisher077@github.com>
2019-04-18 13:15:45 -0300

    faltam 3 quests

--- roteiro5/ClassificaAluno.py
+++ roteiro5/ClassificaAluno.py
@@ -1,9 +1,9 @@
 def ClassificaAluno(media, faltas):
     if(faltas <= 10) :
-        if (media >=7):
-            print("APROVADO ")
-        elif (media >= 9.5):
+        if (media >= 9.5):
             print("APROVADO COM LOUVOR")
+        elif (media >=7):
+            print("APROVADO ")
         else:
             print("REPROVADO")
     else:
