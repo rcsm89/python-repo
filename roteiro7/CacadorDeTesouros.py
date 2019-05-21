dc139dfb7b178d0f4a8fb564c963e7eed3f2fe29
puniser077 <punisher077@github.com>
2019-05-21 15:53:33 -0300

    roteiro 7 terminado

--- roteiro7/CacadorDeTesouros.py
+++ roteiro7/CacadorDeTesouros.py
@@ -5,19 +5,21 @@ def cercado(x, y):
 
 
 def caminho(x=0, y=0, pont=0):
-    if all(cercado(x, y)) or mat[x][y] == '#':
+    ca = mat[x][y]
+    if all(cercado(x, y)) or ca == '#':
         pont += dic[mat[x][y]]
         return pont
     else:
-        pont += dic[mat[x][y]]
+        pont += dic[ca]
         mat[x][y] = "#"
-        ponts = [
-            0 if cercado(x, y)[0] else caminho(x + 1, y, pont),  # Baixo
-            0 if cercado(x, y)[2] else caminho(x - 1, y, pont),  # Cima
-            0 if cercado(x, y)[1] else caminho(x, y + 1, pont),  # Direita
-            0 if cercado(x, y)[3] else caminho(x, y - 1, pont)  # Esquerda
-        ]
-        return max(ponts, key=int)
+        pm = max([
+            0 if x+1==m or mat[x+1][y] == "#" else caminho(x + 1, y, pont),  # Baixo
+            0 if x-1<0 or mat[x-1][y] == "#" else caminho(x - 1, y, pont),  # Cima
+            0 if y+1==n or mat[x][y+1] == "#" else caminho(x, y + 1, pont),  # Direita
+            0 if y-1<0 or mat[x][y-1] == "#" else caminho(x, y - 1, pont)  # Esquerda
+        ], key=int)
+        mat[x][y] = ca
+        return pm 
 
 
 m, n = [int(i) for i in input().split()]
