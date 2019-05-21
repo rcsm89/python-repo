c5c726955f1c5f043fca8e19bc13834c58703fe8
puniser077 <punisher077@github.com>
2019-05-21 09:17:51 -0300

    COROA FEITA E LABIRINTO 40%

--- roteiro7/CacadorDeTesouros.py
+++ roteiro7/CacadorDeTesouros.py
@@ -1,12 +1,28 @@
-def caminho(x, y, pont = 0):
-
-        return 0
-
-
-
-
-m, n, mat= [int(i) for i in input().split()],[]
-for i in range(m):
-    mat.append([(j) for j in input().split()])
+def cercado(x, y):
+    # CIMA, DIREITA, BAIXO, ESQUERDA
+    return [x + 1 >= m or mat[x + 1][y] == "#", y + 1 >= n or mat[x][y + 1] == "#", x - 1 < 0 or mat[x - 1][y] == "#",
+            y - 1 < 0 or mat[x][y - 1] == "#"]
 
 
+def caminho(mat, x=0, y=0, pont=0):
+    if all(cercado(x, y)):
+        pont += dic[mat[x][y]]
+        return pont
+    else:
+        pont += dic[mat[x][y]]
+        mat[x][y] = "#"
+        ponts = [
+            0 if cercado(x, y)[0] else caminho(mat, x + 1, y, pont),  # Cima
+            0 if cercado(x, y)[2] else caminho(mat, x - 1, y, pont),  # Baixo
+            0 if cercado(x, y)[1] else caminho(mat, x, y + 1, pont),  # Direita
+            0 if cercado(x, y)[3] else caminho(mat, x, y - 1, pont)  # Esquerda
+        ]
+
+        return max(ponts, key=int)
+
+
+m, n = [int(i) for i in input().split()]
+mat = [list(input()) for j in range(m)]
+# 1 = Bronze, 5 = Prata, 10 = Ouro, 50 = Diamante
+dic = dict([('b', 1), ('p', 5), ('o', 10), ('d', 50), ('.', 0), ('#', 0)])
+print(caminho(mat))
--- roteiro7/CaraOuCoroa.py
+++ roteiro7/CaraOuCoroa.py
@@ -1,9 +1,9 @@
 def calcMoedas(nn):
-    return calcMoedas()
-    return 0    
-
-
-
+    if (nn == 1):
+        return ['C', 'K']
+    else:
+        moedas = ['C', 'K']
+        return [m + i for m in moedas for i in calcMoedas(nn - 1)]
 
 n, d = input().split()
-
+print(len([x for x in calcMoedas(int(n)) if (abs(x.count("C") - x.count("K"))) == int(d)]))
